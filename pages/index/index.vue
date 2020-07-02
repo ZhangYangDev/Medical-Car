2ba57ae2831e00dd4aa88a319dc59635e72f4bb5
ZhangYangDev <1151559314@qq.com>
2020-07-02 22:48:10 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,70 @@
+<template>
+	<view>
+		<page-head title="swiper,可滑动视图"></page-head>
+		<view class="uni-margin-wrap">
+			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item>
+					<view class="swiper-item uni-bg-red">A</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-green">B</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-blue">C</view>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.uni-margin-wrap {
+		width:690rpx;
+		margin:0 30rpx;
+	}
+	.swiper {
+		height: 300rpx;
+	}
+	.swiper-item {
+		display: block;
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
+	}
+
+	.swiper-list {
+		margin-top: 40rpx;
+		margin-bottom: 0;
+	}
+
+	.uni-common-mt{
+		margin-top:60rpx;
+		position:relative;
+	}
+
+	.info {
+		position: absolute;
+		right:20rpx;
+	}
+
+    .uni-padding-wrap {
+        width:550rpx;
+        padding:0 100rpx;
+    }
+</style>
